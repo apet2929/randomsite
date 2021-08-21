c7e579ceb21645787b5048a5c3ef84e765df9787
apet2929 <56840582+apet2929@users.noreply.github.com>
2021-08-21 12:38:19 -0500

    added deleting and adding multiple items

--- src/index.js
+++ src/index.js
@@ -1,54 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 import { useForm } from "react-hook-form";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
+    // localStorage.setItem("items", JSON.stringify(["yee", "2"]));
+    let items = JSON.parse(localStorage.getItem("items"));
     this.state = {
-      yee: "",
+      items: items ? items : [],
     };
   }
 
   onSubmit = (data) => {
+    let yee = this.state.items;
+    yee.push(data.inputItem);
     this.setState({
-      yee: data.name,
+      items: yee,
     });
-    console.log(data);
-  }
+    localStorage.setItem("items", JSON.stringify(yee));
 
-  render(){
+  };
+
+  render() {
+    let deleteItem = (item) => {
+      console.log(item);
+      let yee = this.state.items;
+      console.log(yee);
+      for(var i = 0; i < yee.length; i++){
+        if(yee[i] === item.props.name){
+          yee.splice(i, 1);
+        }
+      }
+      this.setState({
+        items: yee,
+      });
+      localStorage.setItem("items", JSON.stringify(yee));
+      console.log(yee);
+      console.log("done");
+    }
+    let names = this.state.items.map((yee) => {
+      return <Item name={yee} delete={deleteItem} />;
+    });
     
     return (
-        <>
-            <Form onSubmit={ this.onSubmit }/>
-            <Name name={ this.state.yee } />
-        </>
+      <>
+        <Form onSubmit={this.onSubmit} />
+        {names}
+      </>
     );
-  } 
+  }
 }
 
 function Form(props) {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
   return (
     <form onSubmit={handleSubmit(props.onSubmit)}>
-      <input defaultValue="test" {...register("name")} />
+      <input {...register("inputItem")} />
 
       <input type="submit" />
     </form>
-  )
+  );
 }
 
-function Name(props) {
-  return(
-    <p>{ props.name }</p>
-  )
+class Item extends React.Component{
+  
+  render(){
+    let deleteSelf = () => {
+      this.props.delete(this);
+    };
+    return(
+    <div class="item"> 
+      <p class="item">{this.props.name}</p>
+      <button onClick={deleteSelf}>Delete</button>
+    </div>
+    );
+  }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
